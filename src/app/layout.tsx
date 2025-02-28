99808943dd5ba3ce7febcda718f879419344a4bb
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2025-02-28 19:23:59 +0530

    Update metadata title and keywords for Stream Sync branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ import { UserProvider } from '@/context/context'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "YouTube Clone - Enhanced Video Sharing Platform",
+  title: "Stream Sync - Enhanced Video Sharing Platform",
   description: "Experience an upgraded video platform with advanced features like real-time chat, stories, video comments search, and direct sharing. Enjoy a seamless video-sharing and interactive experience, built with modern web technologies.",
-  keywords: "YouTube Clone, video sharing platform, real-time chat, stories, video comments search, direct video sharing, interactive video app, modern web application, enhanced YouTube features",
+  keywords: "YouTube Clone,Stream Sync, video sharing platform, real-time chat, stories, video comments search, direct video sharing, interactive video app, modern web application, enhanced YouTube features",
   author: "Adarsh Sharma",
 };
 
