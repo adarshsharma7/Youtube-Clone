0f2c417813a83787f1cc6ff6c2e4aa13162b388a
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2025-03-01 08:48:12 +0530

    Remove root path from public routes in middleware for streamlined access

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(request: NextRequest) {
 
 
    // Allow access to public routes without authentication
-   const publicPaths = ['/','/sign-in', '/sign-up', '/verify', '/forgetpassword'];
+   const publicPaths = ['/sign-in', '/sign-up', '/verify', '/forgetpassword'];
    const isPublicPath = publicPaths.some((path) => url.pathname.startsWith(path));
    
  
